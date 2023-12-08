90719e0eccebf91a63ea4115e7fda6df29691be6
Gehendra Chaudhary <chaudharygehendra49@gmail.com>
2023-12-08 11:23:53 +0000

    custom template

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from blog.models import Post
 from django import template  # Add this import statement
+from django.shortcuts import render, get_object_or_404
 
 register = template.Library()  # Add this line
 
@@ -11,20 +12,8 @@ def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
     return render(request, "blog/index.html", {"posts": posts})
 
-@register.filter
-def author_details(author, current_user):
-    if not isinstance(author, user_model):
-        # return empty string as safe default
-        return ""
 
-    if author == current_user:
-        return format_html("<strong>me</strong>")
 
-    if author.first_name and author.last_name:
-        name = f"{author.first_name} {author.last_name}"
-    else:
-        name = f"{author.username}"
-
-    if author.email:
-        prefix = format_html('<a href="mailto:{}">', author.email)
-        suffix = format_html("</a>")
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/post-detail.html", {"post": post})
